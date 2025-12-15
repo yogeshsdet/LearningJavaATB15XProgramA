48c1e43980f31cfe4852124b6583df92c66c5700
Yogesh <gaikwadyp@gmail.com>
2025-12-15 13:58:22 +0530

    Odd numbers from the list class adding

---
+++ src/ex_01_Java_Basic/Lab005_OddFromList.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab005_OddFromList {
+    static void main(String[] args) {
+        for(int i = 1; i <=10; i ++){
+            if(i % 2 != 0){
+                System.out.println("ODD numbers between 1 to 10: " + i);
+            }
+
+        }
+    }
+}
