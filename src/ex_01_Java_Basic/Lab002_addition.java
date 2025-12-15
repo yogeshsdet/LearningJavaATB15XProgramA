0da476ce1c66bc40f14f825ad693799a3879ada2
Yogesh <gaikwadyp@gmail.com>
2025-12-15 13:00:32 +0530

    Add the addtion program into GIT

---
+++ src/ex_01_Java_Basic/Lab002_addition.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab002_addition {
+    static void main(String[] args) {
+        int i = 4;
+        int ii = 10;
+        int result = i + ii;
+
+            System.out.println("Addition of both values = " + result);
+        }
+
+}
