76e210220171a3674c4f610bcf64897557500fc1
Yogesh <gaikwadyp@gmail.com>
2025-12-15 13:40:18 +0530

    Reserve and EvenOdd class adding

---
+++ src/ex_01_Java_Basic/Lab_003_Reserve.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab_003_Reserve {
+    static void main(String[] args) {
+        String str = "Automation";
+        String reverse = new StringBuffer(str).reverse().toString();
+        System.out.println(reverse);
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab_004_EvenOdd.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab_004_EvenOdd {
+    static void main(String[] args) {
+        int i = 13;
+        if(i % 2 == 0){
+            System.out.println("EVEN NUMBER");
+        }
+        else
+            System.out.println("NUMBER IS ODD NUMBER");
+    }
+}
