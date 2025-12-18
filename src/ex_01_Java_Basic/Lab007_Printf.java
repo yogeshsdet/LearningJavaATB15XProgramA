2d62933d5f497f81967687465a6920f542d67d81
Yogesh <gaikwadyp@gmail.com>
2025-12-18 20:11:21 +0530

    Table of 9 using with printf

---
+++ src/ex_01_Java_Basic/Lab007_Printf.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basic;
+
+public class Lab007_Printf {
+    static void main(String[] args) {
+        int t = 9;
+        System.out.println("Table of 9: ");
+        System.out.printf("%d * 1 = %d%n", t, (t * 1));
+        System.out.printf("%d * 2 = %d%n", t , (t * 2));
+        System.out.printf("%d * 3 = %d%n", t , (t * 3));
+        System.out.printf("%d * 4 = %d%n", t , (t * 4));
+        System.out.printf("%d * 5 = %d%n", t , (t * 5));
+        System.out.printf("%d * 6 = %d%n", t , (t * 6));
+        System.out.printf("%d * 7 = %d%n", t , (t * 7));
+        System.out.printf("%d * 8 = %d%n", t , (t * 8));
+        System.out.printf("%d * 9 = %d%n", t , (t * 9));
+        System.out.printf("%d * 10 = %d%n", t , (t * 10));
+
+    }
+}
