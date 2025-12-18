4e6b986470ddfb21481353ac0578dc21a7fa2b32
Yogesh <gaikwadyp@gmail.com>
2025-12-18 13:19:20 +0530

    Table of 9 using with println

---
+++ src/ex_01_Java_Basic/Lab006_Table.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab006_Table {
+    static void main(String[] args) {
+        int i = 9;
+        System.out.println("Table of 9:");
+        for(int a = 1; a <= 10; a++){
+            System.out.println("9" + " * " + a + " = " + i * a );
+        }
+    }
+}
