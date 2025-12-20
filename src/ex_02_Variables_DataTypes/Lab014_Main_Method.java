89b5ce387a9dd0d4f9e3ef7147b65c44b98faf6e
Yogesh <gaikwadyp@gmail.com>
2025-12-20 08:26:06 +0530

    Lab014 & Lab015 class files

---
+++ src/ex_02_Variables_DataTypes/Lab014_Main_Method.java
@@ -0,0 +1,17 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab014_Main_Method {
+    public static void main(String[] args) {
+
+    }
+    /*
+    public static void main(String[] args) {
+
+
+    }
+    public static void main(String[] args) {
+
+    }*/
+}
+
+
---
+++ src/ex_02_Variables_DataTypes/Lab015_Variables.java
@@ -0,0 +1,14 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab015_Variables {
+    static void main(String[] args) {
+
+
+        //data_type variable_name = variable_value
+        //variable - local variable
+        byte age_pramod = 45;
+        age_pramod = 112;
+        age_pramod = 119;
+        System.out.println(age_pramod);
+    }
+}
