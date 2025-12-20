ecb790dbf4514086a3de2b4629e13ee4520b9ddb
Yogesh <gaikwadyp@gmail.com>
2025-12-20 08:12:56 +0530

    Lab013_Println_vs_Print class

---
+++ src/ex_02_Variables_DataTypes/Lab013_Println_vs_Print.java
@@ -0,0 +1,11 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab013_Println_vs_Print {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Bye");
+
+        System.out.print("Hi");
+        System.out.print("Bye");
+    }
+}
