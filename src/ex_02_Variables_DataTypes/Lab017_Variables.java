5cfc4b7af11321b75edcc27e0eaed7ff58b9908f
Yogesh <gaikwadyp@gmail.com>
2025-12-21 08:33:56 +0530

    variable class

---
+++ src/ex_02_Variables_DataTypes/Lab017_Variables.java
@@ -0,0 +1,13 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab017_Variables {
+    public static void main(String[] args) {
+        // I want to the age of a person
+        // byte, short, int?
+
+        int age = 122;
+        short age_s = 122;
+        byte age_b = 122;
+        short s = 128;
+    }
+}
