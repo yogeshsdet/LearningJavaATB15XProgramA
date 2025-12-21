ae04fcb0d71fd5ff0ea28ce2e23bf6e96c65d93c
Yogesh <gaikwadyp@gmail.com>
2025-12-21 07:37:49 +0530

    variable class

---
+++ src/ex_02_Variables_DataTypes/Lab016_Variables.java
@@ -0,0 +1,10 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab016_Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        int a1 = -10;
+        a = 65+1;
+        System.out.println(a);
+    }
+}
