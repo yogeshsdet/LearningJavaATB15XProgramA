9a61768f787a55380101a90cb4f3e443b355e536
Yogesh <gaikwadyp@gmail.com>
2025-12-21 08:58:15 +0530

    Numeric data types class

---
+++ src/ex_02_Variables_DataTypes/Lab022_Numerics_Data_Types.java
@@ -0,0 +1,36 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab022_Numerics_Data_Types {
+    public static void main(String[] args) {
+
+
+    byte b = 10;
+    short s = 110;
+    int i = 1111;
+    char c = 'A';
+    char c1 = '@';
+    char c2 = '_';
+
+    long phone = 9090989898l;
+    long phone2 = 8989998889L;
+
+    float f = 3.12f;
+    float f1 = 34.33F;
+
+    double d = 2.2222222;
+
+        System.out.println("Byte value " + b);
+        System.out.println("Short value " + s);
+        System.out.println("Int value " + i);
+        System.out.println("Char value " + c);
+        System.out.println("Char one value " + c1);
+        System.out.println("Char two value " + c2);
+        System.out.println("Long value " + phone );
+        System.out.println("Long two value " + phone2);
+        System.out.println("Float one value " + f);
+        System.out.println("Float two value " + f1);
+        System.out.println("Double value " + d);
+
+}
+
+}
