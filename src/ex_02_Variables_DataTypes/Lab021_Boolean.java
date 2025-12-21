afca28c6e2f47848297568ab90738b5c5c991fee
Yogesh <gaikwadyp@gmail.com>
2025-12-21 08:48:12 +0530

    boolean

---
+++ src/ex_02_Variables_DataTypes/Lab021_Boolean.java
@@ -0,0 +1,10 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab021_Boolean {
+    public static void main(String[] args) {
+        boolean yogesh_have_offer = true;
+        boolean yogesh_SDET = true;
+        System.out.println("Yogesh have an offer " + yogesh_have_offer);
+        System.out.println("Yogesh role is SDET " + yogesh_SDET);
+    }
+}
