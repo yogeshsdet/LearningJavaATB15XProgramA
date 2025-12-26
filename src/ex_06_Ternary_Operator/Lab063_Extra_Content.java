7fbcae84cb7d25083f7e3ff5c70f4feb48241035
Yogesh <gaikwadyp@gmail.com>
2025-12-26 09:07:38 +0530

    class for boolean

---
+++ src/ex_06_Ternary_Operator/Lab063_Extra_Content.java
@@ -0,0 +1,9 @@
+package ex_06_Ternary_Operator;
+
+public class Lab063_Extra_Content {
+    public static void main() {
+        int a = 10;
+        boolean result = a >= 10 ? true : false;
+        System.out.println(result);
+    }
+}
