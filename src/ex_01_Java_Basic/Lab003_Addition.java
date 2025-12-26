6e88c62ed50e73e061c4fb88846fc0982e544a36
Yogesh <gaikwadyp@gmail.com>
2025-12-26 08:54:00 +0530

    test

---
+++ src/ex_01_Java_Basic/Lab003_Addition.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Addition {
+    public static void main(String[] args) {
+        int i = 120;
+        int j = 340;
+        int result = i + j;
+        System.out.println("Addition of 120 + 340: " + result);
+
+    }
+    void Addition(int a,int b){
+
+            int i = a;
+            int j = b;
+            int result1 = i - j;
+            System.out.println("subtraction of 120 - 340: " + result1);
+        }
+
+}
---
+++ src/ex_02_Variables_DataTypes/Lab023_Byte.java
@@ -0,0 +1,14 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab023_Byte {
+    static void main(String[] args) {
+        long age = 10L;
+        char c = 'C';
+        System.out.println(age);
+        System.out.println(c);
+
+        String y = "Yogesh"; //Non-primitive data type
+
+        
+    }
+}
