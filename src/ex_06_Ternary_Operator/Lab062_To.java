4e5fe90e9a07cb2b9e04a11164957bbaf3994857
Yogesh <gaikwadyp@gmail.com>
2025-12-26 09:12:27 +0530

    Ternary operator programs

---
+++ src/ex_06_Ternary_Operator/Lab062_To.java
@@ -0,0 +1,9 @@
+package ex_06_Ternary_Operator;
+
+public class Lab062_To {
+    public static void main() {
+        int age = 19;
+        String canAmolGotoGoa = age > 18 ? "Yes" : "No";
+        System.out.println(canAmolGotoGoa);
+    }
+}
---
+++ src/ex_06_Ternary_Operator/Lab063_To_P1.java
@@ -0,0 +1,9 @@
+package ex_06_Ternary_Operator;
+
+public class Lab063_To_P1 {
+    public static void main() {
+        int number = -5;
+        String result = number > 0 ? "+ve" : "-ve";
+        System.out.println(result);
+    }
+}
