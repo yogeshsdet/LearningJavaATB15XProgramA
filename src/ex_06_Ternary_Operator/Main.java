a4251fa2415a8cc0ba8ac11adb1ff251dcaeef8f
Yogesh <gaikwadyp@gmail.com>
2025-12-29 15:55:58 +0530

    Ternary operator programs

---
+++ src/ex_06_Ternary_Operator/Main.java
@@ -0,0 +1,10 @@
+package ex_06_Ternary_Operator;
+
+
+    class Main {
+        public static void main(String args[]) {
+            int t=2;
+            System.out.println(t);
+        }
+    }
+
