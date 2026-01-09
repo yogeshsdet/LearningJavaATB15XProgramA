4c5019de4b33c0db7d10da650fd7edfbe275ed5a
Yogesh <gaikwadyp@gmail.com>
2026-01-09 17:16:07 +0530

    variable programs

---
+++ src/ex_02_Variables_DataTypes/Lab027_Multiple_Variable.java
@@ -0,0 +1,13 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab027_Multiple_Variable {
+    public static void main(String[] args) {
+        int a = 10, b = 23, c = 90;
+        int _ = 10, $ = 34;
+        //java: underscore not allowed here error showing while printing the _ variable value.
+        //System.out.println("Print _ value: " + _);
+
+        System.out.println("Print $ value: " + $);
+
+    }
+}
---
+++ src/ex_02_Variables_DataTypes/Lab_026_Local_Variable.java
@@ -0,0 +1,14 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab_026_Local_Variable {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = 10;
+        short s = 10;
+        long l = 4234234L;
+        float f = 3.14F;
+        double d = 34.3333;
+        boolean b1 = true;
+        char c ='A';
+    }
+}
