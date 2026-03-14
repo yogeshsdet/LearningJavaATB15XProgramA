4bf7f0d60c3fe6c30bfb1260f963fec20ff6a9e7
Yogesh <gaikwadyp@gmail.com>
2026-03-14 10:58:01 +0530

    While programs

--- src/ex_11_While/Lab123_While_Guessing_Game.java
+++ src/ex_11_While/Lab123_While_Guessing_Game.java
@@ -1,4 +1,40 @@
 package ex_11_While;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Lab123_While_Guessing_Game {
+    public static void main(String[] args) {
+
+        //Guess a number from 1 to 100
+        //n = 56
+        //90, number <, 50, number > , 55, -56 -times = 9
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100);
+        System.out.println(numberToGuess);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int guess;
+        int attempts = 0;
+
+        while(true){
+            if(!scanner.hasNextInt()){
+                System.out.println("Invalid input! Please enter integer number only!");
+                scanner.next(); //Consume invalid token
+                continue;
+            }
+            guess = scanner.nextInt();
+            attempts++;
+            if(guess < numberToGuess){
+                System.out.println("Too low, Try again!");
+            }else if(guess > numberToGuess){
+                System.out.println("Too High, Try again");
+            }else {
+                System.out.println("Correct! You have token -> " + attempts);
+                break;
+            }
+
+        }
+
+    }
 }
