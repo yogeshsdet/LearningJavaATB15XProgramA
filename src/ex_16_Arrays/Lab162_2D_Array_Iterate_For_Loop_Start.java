77f5ac54c7ff7bd2c9e00b6647d7f1ef87e1f96a
Yogesh <gaikwadyp@gmail.com>
2026-05-15 17:09:26 +0530

    Arrays programs1

---
+++ src/ex_16_Arrays/Lab162_2D_Array_Iterate_For_Loop_Start.java
@@ -0,0 +1,17 @@
+package ex_16_Arrays;
+
+public class Lab162_2D_Array_Iterate_For_Loop_Start {
+    public static void main(String[] args){
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                System.out.print("W" + "|");
+            }
+            System.out.println("");
+        }
+    }
+}
