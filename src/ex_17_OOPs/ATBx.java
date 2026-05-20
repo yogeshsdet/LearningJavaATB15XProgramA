dfb6b60f3fbdfd813b4a91a037f6ee4760ab6a3d
Yogesh <gaikwadyp@gmail.com>
2026-05-20 19:30:30 +0530

    OOPs

---
+++ src/ex_17_OOPs/ATBx.java
@@ -0,0 +1,19 @@
+package ex_17_OOPs;
+
+public class ATBx {
+    //Attributes
+    String name;
+    int age;
+    String gender;
+    int salary;
+    long phone_no;
+    String address;
+    String courseName;
+    String paymentID;
+    // Behaviour
+    void speak(){}
+    void eat(){}
+    void sleep(){};
+    void doAssignment(){};
+    void watchRecordings(){};
+}
